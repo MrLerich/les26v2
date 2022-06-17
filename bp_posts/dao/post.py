a02924716ee01bdbf8c9046a71587e5409b7e441
MrLerich <mrlerich@gmail.com>
2022-06-17 13:54:25 +0300

    write post and postDAO

--- bp_posts/dao/post.py
+++ bp_posts/dao/post.py
@@ -1,3 +1,33 @@
-#класс который хранит данные о посте "абстракция поста"
+# класс который хранит данные о посте "абстракция поста"
 class Post:
-    pass
+    '''Абстракция постов для использования в DAO'''
+    def __init__(self,
+                 pk: int = 0,
+                 poster_name: str = '',
+                 poster_avatar='',
+                 pic='',
+                 content: str = '',
+                 views_count: int = 0,
+                 likes_count: int = 0
+                 ):
+        self.pk = pk
+        self.poster_name = poster_name
+        self.poster_avatar = poster_avatar
+        self.pic = pic
+        self.content = content
+        self.views_count = views_count
+        self.likes_count = likes_count
+
+    def __repr__(self):
+        return f'Post(' \
+               f'{self.pk}, ' \
+               f'{self.poster_name}, ' \
+               f'{self.poster_avatar}, ' \
+               f'{self.pic}, ' \
+               f'{self.content}, ' \
+               f'{self.views_count}, ' \
+               f'{self.likes_count}, ' \
+               f')'
+
+
+
--- bp_posts/dao/post_dao.py
+++ bp_posts/dao/post_dao.py
@@ -1,3 +1,88 @@
-#класс который будет делать все манипуляции с нашими данными почувствова
+# класс который будет делать все манипуляции с нашими данными постов
+
+import json
+from json import JSONDecodeError
+
+from post import Post
+from exceptions.data_exceptions import DataSourceError
+
+
 class PostDAO:
-    pass
+    '''Менеджер абстракции постов'''
+
+    def __init__(self, path):
+        self.path = path
+
+    def _load_data(self) -> list[dict]:
+        '''Загружает данные из JSON и возвращает список словарей'''
+
+        try:
+            with open(self.path, 'r', encoding='utf-8') as file:
+                posts_data = json.load(file)
+        except (FileNotFoundError, JSONDecodeError):
+            raise DataSourceError(f'Не удалось получить данные из файла. Проверьте источник данных.')
+
+        return posts_data
+
+    def _load_posts(self) -> list:
+        '''Возвращает список экземпляров Post'''
+
+        posts_data = self._load_data()
+        list_of_posts = [Post(**post_data) for post_data in posts_data]
+
+        return list_of_posts
+
+    def get_all(self) -> list:
+        '''Получает все посты'''
+
+        posts = self._load_posts()
+
+        return posts
+
+    def get_by_pk(self, pk: int) -> list:
+        '''Получает пост по его pk'''
+
+        if type(pk) != int:
+            raise TypeError('pk должен быть числом')
+
+        posts = self._load_posts()
+
+        post = [post for post in posts if post.pk == pk]
+        # for post in posts:
+        #     if post.pk == pk:
+        #         return post
+        return post
+
+    def search_by_content(self, substring: str) -> list:
+        '''Ищет посты, по входящему в него контенту(substring)'''
+
+        if type(substring) != str:
+            raise TypeError('substring должен быть стоковым значением')
+
+        substring = substring.lower()
+
+        posts = self._load_posts()
+        matching_posts = [post for post in posts if substring in post.content.lower()]
+
+        return matching_posts
+
+    def get_by_poster(self, username: str) -> list:
+        '''Получает все посты пользователя'''
+        if type(username) != str:
+            raise TypeError('username должен быть стоковым значением')
+
+        username = username.lower()
+
+        posts = self._load_posts()
+
+        matching_posts = [post for post in posts if post.poster_name.lower() == username]
+
+        return matching_posts
+
+# pd = PostDAO('../../data/posts.json')
+# pp(pd._load_posts())
+# pp(pd._load_data())
+# pp(pd.get_all())
+# pp(pd.search_by_content('лампочка'))
+# pp(pd.get_by_pk(5))
+# pp(pd.get_by_poster('leo'))
